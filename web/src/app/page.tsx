76ef26f33fe0cc4cda1bb69a787e7b469fc59818
kuaifan <aipaw@live.cn>
2025-05-09 11:17:51 +0800

    no message

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold">App Store</h1>
+      </div>
+    </main>
+  );
+}
